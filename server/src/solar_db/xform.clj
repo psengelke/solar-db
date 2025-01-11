1a923fa640add751254b6b91708a50882f9e8d60
Paul Engelke <paul@htihospitality.tech>
2025-01-11 16:15:36 +0200

    Added xforms.clj

---
+++ server/src/solar_db/xform.clj
@@ -0,0 +1,40 @@
+(ns solar-db.xform
+  (:require [clojure.string :as str]))
+
+(defn- kebab-case->camel-case
+  "Converts a string of the form `a-b-c` to `aBC`."
+  [s]
+  (let [parts (str/split s #"-")]
+    (if (seq parts)
+      (->> parts rest
+           (map str/capitalize)
+           (cons (first parts))
+           (str/join)))))
+
+(defn- camel-case->kebab-case
+  "Transforms a string of the form `aBC` to `a-b-c`."
+  [s]
+  (as-> s $
+        (str/split $ #"(?=[A-Z])")
+        (map str/lower-case $)
+        (str/join "-" $)))
+
+(defn keyword->str
+  "Transforms a keyword of the form `:my-namespace/my-name` to `myNamespace_myName`."
+  [k]
+  (when (keyword? k)
+    (let [ns (-> k namespace str)
+          n  (-> k name str)]
+      (->> [ns n]
+           (map kebab-case->camel-case)
+           (remove str/blank?)
+           (str/join "_")))))
+
+(defn str->keyword
+  "Transforms a string of the form `myNamespace_myName` to `:my-namespace/my-name`."
+  [s]
+  (when (string? s)
+    (let [parts (as-> s $ (str/split $ #"_") (map camel-case->kebab-case $))]
+      (if (= 2 (count parts))
+        (keyword (first parts) (second parts))
+        (keyword (first parts))))))
